0744198020e171045327a65cc8bdd5b9d1285f72
Dirk Roessler <roessler@gempa.de>
2022-09-15 12:02:32 +0200

    [install-fdsnws.sh] Ask for user before pip

--- src/system/apps/seiscomp/share/deps/rhel/7/install-fdsnws.sh
+++ src/system/apps/seiscomp/share/deps/rhel/7/install-fdsnws.sh
@@ -1,5 +1,10 @@
 yum install python36-dateutil python36-pip
 
+echo "user name for pip install (sysop):"
+read user
+
+if  [ -z "$user" ]; then user=sysop; fi
+
 # install Python packages using pip
 echo "installing packages for user " $user
 su - $user -c 'python3 -m pip install --user twisted'
