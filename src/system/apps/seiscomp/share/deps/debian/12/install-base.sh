4f97a972d29476a547e78da2a9a4c5686741024b
Jan Becker <jabe@gempa.de>
2023-06-26 10:52:15 +0200

    [deps] Add Debian 12

---
+++ src/system/apps/seiscomp/share/deps/debian/12/install-base.sh
@@ -0,0 +1,1 @@
+apt install libxml2 libboost-filesystem1.74.0 libboost-iostreams1.74.0 libboost-thread1.74.0 libboost-program-options1.74.0 libboost-regex1.74.0 libboost-system1.74.0 libssl3 libncurses5 libmariadb3 libpq5 libpython3.11 python3-numpy
