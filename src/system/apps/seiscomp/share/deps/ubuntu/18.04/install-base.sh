b653472a52420cd4aeb7970a1346320072a52620
Stephan Herrnkind <herrnkind@gempa.de>
2023-12-14 12:56:58 +0100

    [deps] Update software dependencies - RHEL 7: Swich Qt4 to Qt5 - UB 18: Remove, EOL reached

--- src/system/apps/seiscomp/share/deps/ubuntu/18.04/install-base.sh
+++
@@ -1,1 +0,0 @@
-apt-get install libxml2 libboost-filesystem1.65.1 libboost-iostreams1.65.1 libboost-thread1.65.1 libboost-program-options1.65.1 libboost-regex1.65.1 libboost-signals1.65.1 libboost-system1.65.1 libssl1.0.0 libncurses5 libmysqlclient20 libpq5 libpython3.6 python3-numpy
--- src/system/apps/seiscomp/share/deps/ubuntu/18.04/install-mariadb-server.sh
+++
@@ -1,1 +0,0 @@
-apt-get install mariadb-server mariadb-client
